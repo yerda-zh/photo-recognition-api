65b6f52c41545f71081c310b412ae034e8e82b3d
yerda-zh <yerdaulet.04@gmail.com>
2023-08-02 22:42:07 +0500

    added env

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ const image = require('./controllers/image');
 const knex = require('knex')({
 client: 'pg',
 connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'yerda.Sql',
-    database : 'face-recognition'
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host : process.env.DATABASE_HOST,
+    port: 5432,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PW,
+    database : process.env.DATABASE_DB
 }
 });
 
